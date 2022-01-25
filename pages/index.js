c46af278e5ce5a635e467e2dee5a0e4bd7b8ca24
felipeverones <felipe_verones@hotmail.com>
2022-01-25 17:46:44 -0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
 import appConfig from '../config.json'
+import React from 'react';
+import { useRouter } from 'next/router'
 
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
 
 function Title(props) {
   console.log(props.children);
@@ -64,11 +38,13 @@ function Title(props) {
 export default HomePage; */
 
 export default function PaginaInicial() {
-    const username = 'peas';
-  
+    //const username = 'felipeverones';
+    const [username, setUsername] = React.useState('felipeverones')
+    const roteamento = useRouter()
+
     return (
       <>
-        <GlobalStyle />
+        {/* <GlobalStyle /> */}
         <Box
           styleSheet={{
             display: "flex",
@@ -103,6 +79,12 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento){
+                infosDoEvento.preventDefault();
+                console.log("alguem submeteu o form")
+                roteamento.push('/chat')
+                //window.location.href = '/chat' //jeito tradicional do navegador
+              }}
               styleSheet={{
                 display: "flex",
                 flexDirection: "column",
@@ -123,8 +105,29 @@ export default function PaginaInicial() {
               >
                 {appConfig.name}
               </Text>
-
+              
+              {/* <input 
+              type="text"
+              value={username}
+              onChange={function (evento){
+                console.log("usuário digitou")
+                //onde ta o valor?
+                const valor = evento.target.value
+                // trocar o valor da variável
+                //através do React e avisa quem precisa
+                setUsername(valor)
+              }}
+              /> */}
               <TextField
+              value={username}
+              onChange={function (evento){
+                console.log("usuário digitou")
+                //onde ta o valor?
+                const valor = evento.target.value
+                // trocar o valor da variável
+                //através do React e avisa quem precisa
+                setUsername(valor)
+              }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
